ef789839a399b13342893cc3531de58d8a3ed710
alimarchal <alirazamarchal@hotmail.com>
2024-12-03 10:11:36 +0500

    added changes in filter batch -2

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -67,14 +67,36 @@ class ReportController extends Controller
     }
     
     
-    public function result_batch_two()
+    public function result_batch_two(Request $request)
     {
         $user = Auth::user();
         if ($user->email == "dh_hrd@bankajk.com" || $user->email == "hrd@bankajk.com") {
+            $district_of_domicile = ['Muzaffarabad', 'Jhelum Valley', 'Neelum', 'Refugee'];
+
+            if (!empty($request->filter['region'])) {
+                switch ($request->filter['region']) {
+                    case "Rawalakot":
+                        $district_of_domicile = ['Sudhanoti', 'Poonch', 'Bagh', 'Haveli'];
+                        break;
+                    case "Kotli":
+                        $district_of_domicile = ['Kotli'];
+                        break;
+                    case "Mirpur":
+                        $district_of_domicile = ['Mirpur', 'Bhimber'];
+                        break;
+                    default:
+                        // Reset to default value for unknown regions
+                        $district_of_domicile = ['Muzaffarabad', 'Jhelum Valley', 'Neelum', 'Refugee'];
+                }
+            }
+
+
+//            dd($district_of_domicile);
+
             $users = User::where('profile_status', 1)->where('status', 'Shortlisted')
-//            ->where('batch_no','Batch-02')
-            ->where('test_date','2024-11-24')
-//            ->whereIn('district_of_domicile',['Mirpur','Bhimber','Kotli'])
+            ->where('batch_no','Batch-02')
+//            ->where('test_date','2024-11-24')
+            ->whereIn('district_of_domicile',$district_of_domicile)
 //            ->whereIn('district_of_domicile',['Bagh','Poonch','Sudhanoti','Haveli'])
 //            ->whereNotIn('district_of_domicile',['Jhelum Valley','Muzaffarabad'])
 //            ->whereIn('id',[808,869,897,928,946,983,994,1008,1017,1024,1034,1073,1093,1127,1137,1138,1149,1161,1162,1194,1199,1200,1230,1269,1332,1352,1356,1363,1365,1389,1402,1438,1439,1462,1488,1508,1528,1541,1582,1584,1585,1650,1668,1674,1678,1685,1690,1697,1705,1711,1712,1723,1726,1729,1761,1789,1802,1823,1848,1855,1863,1881,1888,1891,1909,1912,1913,1926,1950,1958,1987,2058,2102])
--- resources/views/candidate/result_batch_two.blade.php
+++ resources/views/candidate/result_batch_two.blade.php
@@ -76,22 +76,68 @@
         </style>
 
     @endpush
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl uppercase text-gray-800 dark:text-gray-200 leading-tight inline-block">
-            Print
-        </h2>
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight inline-block">
+                {{ __('Result Batch-02') }}
+            </h2>
+
+            <div class="flex justify-center items-center float-right">
+
+                <a href="#"
+                   class="flex items-center px-4 py-1.5 text-gray-600 bg-white border rounded-lg focus:outline-none hover:bg-gray-100 transition-colors duration-200 transform dark:text-gray-200 dark:border-gray-200  dark:hover:bg-gray-700 ml-2">
+                    Back
+                </a>
+
+                <a href="javascript:;" id="toggle"
+                   class="flex items-center px-4 py-2 text-gray-600 bg-white border rounded-lg focus:outline-none hover:bg-gray-100 transition-colors duration-200 transform dark:text-gray-200 dark:border-gray-200  dark:hover:bg-gray-700 ml-2"
+                   title="Members List">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                         stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z"/>
+                    </svg>
+                </a>
+
+            </div>
+
+        </x-slot>
+        <div class="max-w-8xl mx-auto mt-12 px-4 sm:px-2 lg:px-8 print:hidden " style="display: none" id="filters">
+            <div class="rounded-xl p-4 bg-white shadow-lg">
 
 
-        <div class="flex justify-center items-center float-right">
-            <a href="javascript:;" onclick="window.print()"
-               class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                          d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0 1 10.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0 .229 2.523a1.125 1.125 0 0 1-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0 0 21 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 0 0-1.913-.247M6.34 18H5.25A2.25 2.25 0 0 1 3 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 0 1 1.913-.247m10.5 0a48.536 48.536 0 0 0-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5Zm-3 0h.008v.008H15V10.5Z"/>
-                </svg>
-            </a>
+
+                <form method="GET" action="#">
+                    <div class="mt-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+
+                        <!-- Religion -->
+                        <div>
+                            <x-label for="region" value="{{ __('Region') }}" />
+                            <select name="filter[region]" id="region" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full">
+                                <option value="">Select a region</option>
+                                <option value="Muzaffarabad" {{ request('filter.religion') === 'Muzaffarabad' ? 'selected' : '' }}>Muzaffarabad</option>
+                                <option value="Rawalakot" {{ request('filter.religion') === 'Rawalakot' ? 'selected' : '' }}>Rawalakot</option>
+                                <option value="Kotli" {{ request('filter.religion') === 'Kotli' ? 'selected' : '' }}>Kotli</option>
+                                <option value="Mirpur" {{ request('filter.religion') === 'Mirpur' ? 'selected' : '' }}>Mirpur</option>
+                            </select>
+                        </div>
+
+
+
+
+
+
+
+                    </div>
+
+                    <div class="mt-4">
+                        <x-button class="bg-indigo-500 text-white">
+                            {{ __('Apply Filters') }}
+                        </x-button>
+                    </div>
+                </form>
+
+            </div>
         </div>
-    </x-slot>
 
     <div class="py-6">
         <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
@@ -418,4 +464,22 @@
         </div>
     </div>
 
+    @push('modals')
+        <script>
+
+            const targetDiv = document.getElementById("filters");
+            const btn = document.getElementById("toggle");
+            btn.onclick = function () {
+                if (targetDiv.style.display !== "none") {
+                    targetDiv.style.display = "none";
+                } else {
+                    targetDiv.style.display = "block";
+                }
+            };
+
+            function redirectToLink(url) {
+                window.location.href = url;
+            }
+        </script>
+    @endpush
 </x-app-layout>
